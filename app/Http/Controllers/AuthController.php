9f473b785f8067f9c0bd401cc404b5a6a86de760
buingocdodev04@gmail.com <8.12a5.dobuingoc@gmail.com>
2024-09-25 00:07:18 +0700

    update-dangky

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Rules\Captcha;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-
-
+use App\Jobs\SendPasswordResetEmail;
+use App\Mail\ResetPasswordMail;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\PasswordReset;
 class AuthController extends Controller
 {
     public function showLogin()
@@ -18,7 +23,8 @@ class AuthController extends Controller
         $title = "Đăng nhập tài khoản";
 
         return view("auths.login", compact("title"));
-    }  public function redirectToGoogle()
+    }
+    public function redirectToGoogle()
     {
         return Socialite::driver('google')->redirect();
     }
@@ -28,92 +34,149 @@ class AuthController extends Controller
 
             $user = Socialite::driver('google')->user();
 
-            $finduser = User::where('google_id', $user->id)->orWhere('email',$user->email)->first();
+            $finduser = User::where('google_id', $user->id)->orWhere('email', $user->email)->first();
 
             if ($finduser) {
-                if($finduser->google_id){
+                if ($finduser->google_id) {
                     $finduser->google_id = $user->id;
                     $finduser->save();
                 }
                 Auth::login($finduser);
                 request()->session()->regenerate();
-                return redirect()->route('admin.dashboard');
+                return redirect()->intended('dashboard');
             } else {
-                return redirect()->route("login")->with(["message"=> "Tài khoản chưa tồn tại"]);
+                return redirect()->route("login")->with(["message" => "Tài khoản chưa tồn tại"]);
             }
         } catch (\Exception $e) {
             dd($e->getMessage());
         }
     }
-    public function login(Request $request){
-         if($request->isMethod("POST")){
+    public function login(Request $request)
+    {
+        if ($request->isMethod("POST")) {
             $request->validate([
                 "username" => "required",
                 "password" => "required",
                 "g-recaptcha-response" => new Captcha(),
-            ],[
+            ], [
                 "required" => ":attribute không được để trống",
-            ],[
+            ], [
                 "username" => "Tên đăng nhập",
                 "password" => "Mật khẩu",
                 "g-recaptcha-response" => "Xác nhận captcha",
             ]);
-           $data = $request->except("_token","g-recaptcha-response");
-           $user = User::query()->where("username",$data["username"])->first(); 
-           if($user && Hash::check($data["password"],$user->password)){
-            Auth::login($user);
-            request()->session()->regenerate();
-            return redirect()->route("showMessage");
-           }
-           else if( $user && !Hash::check($data["password"],$user->password)) {
-            return redirect()->route("showLogin")->with('message',"Tài khoản mật khẩu không đúng !");
-           }
-           else {
-            return redirect()->route("showLogin")->with('message',"Tài khoản mật khẩu không tồn tại !");
-           }
-         }
+            $data = $request->except("_token", "g-recaptcha-response");
+            $user = User::query()->where("username", $data["username"])->first();
+            if ($user && Hash::check($data["password"], $user->password)) {
+                Auth::login($user);
+                request()->session()->regenerate();
+                return redirect()->route("showMessage");
+            } else if ($user && !Hash::check($data["password"], $user->password)) {
+                return redirect()->route("showLogin")->with('message', "Tài khoản mật khẩu không đúng !");
+            } else {
+                return redirect()->route("showLogin")->with('message', "Tài khoản mật khẩu không tồn tại !");
+            }
+        }
     }
 
-    public function showRegister (){
+    public function showRegister()
+    {
         $title = "Đăng ký tài khoản";
 
-        return view("auths.register",compact("title"));
+        return view("auths.register", compact("title"));
     }
 
-    public function register(Request $request){
-        if($request->isMethod("POST")){
+    public function register(Request $request)
+    {
+        if ($request->isMethod("POST")) {
             $request->validate([
                 "username" => "required|unique:users,username",
                 "email" => "required|unique:users,email",
                 "password" => "required|min:8",
                 "g-recaptcha-response" => new Captcha(),
-            ],[
+            ], [
                 "required" => ":attribute không được để trống",
                 "unique" => ":attribute đã tồn tại",
                 "min" => "Mật khẩu phải có ít nhất 8 ký tự",
-            ],[
+            ], [
                 "username" => "Tên đăng nhập",
                 "email" => "Email",
                 "password" => "Mật khẩu",
             ]);
-           $data = $request->except("_token","g-recaptcha-response");
+            $data = $request->except("_token", "g-recaptcha-response");
             $user = User::query()->create([
                 "username" => $data["username"],
                 "password" => Hash::make($data["password"]),
                 "email" => $data["email"]
             ]);
-
-            if($user){
-                    Auth::login($user);
-                    request()->session()->regenerate();
+            if ($user) {
+                Auth::login($user);
+                request()->session()->regenerate();
                 return redirect()->route("showMessage");
             }
         }
     }
-    public function logout(){   
+    public function logout()
+    {
         Auth::logout();
         request()->session()->invalidate();
         request()->session()->regenerateToken();
         return redirect()->route("showLogin");
     }
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        if(User::query()->where("email", "=", $request->email)->first()){
+            $status = Password::sendResetLink($request->only('email'));
+            
+            if ($status === Password::RESET_LINK_SENT) {
+                $user = User::query()->where("email", "=", $request->email)->first();
+                $token = app('auth.password.broker')->createToken($user);
+        
+                Mail::to($request->email)->send(new ResetPasswordMail($token,$user->email));
+
+                return back()->with(['status' => __($status)]);
+            } else {
+                return back()->withErrors(['email' => __($status)]);
+            }
+        } else {
+            return back()->withErrors(['email' => "Email của bạn chưa tồn tại"]);
+        }
+      
+
+        
+    }
+    public function showConfirmEmail(){
+        $title = "Quên mật khẩu";
+        return view("auths.confirm_email",compact("title"));
+    }
+    public function createPassword(Request $request){
+        $request->validate([
+            'password' => 'required|min:8',
+            "re_password" => "required|same:password"
+        ],[
+            "required" => ":attribute không được để trống",
+            "password.min" => "Mật khẩu phải dài hơn 8 ký tự",
+            "re_password.same" => "Mật khẩu không trùng khớp",
+           
+        ],[
+            "password" => "Mật khẩu"
+        ]);
+        $status = Password::reset(
+            $request->only('email', 'password', 're-password', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->save();
+                  
+                Auth::login($user);
+            }
+        );
+       
+        return $status === Password::PASSWORD_RESET
+                    ? redirect()->route('showLogin')->with('status', __($status))
+                    : back()->withErrors(['email' => [__($status)]]);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,18 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DasboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ajax\ChangeStatusAjax;
+use App\Http\Controllers\ajax\GetLocaitonAjax;
+
+use App\Http\Controllers\backend\AttributeController;
+use App\Http\Controllers\backend\AttributeValueController;
+use App\Http\Controllers\backend\DashBoardController;
+use App\Http\Controllers\backend\PostController;
+use App\Http\Controllers\backend\ProductCatelogueController;
+use App\Http\Controllers\Backend\UserCatelogueController;
+use App\Http\Controllers\Backend\UserController;
+
+use App\Http\Controllers\PostCatelogueController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,8 +51,75 @@ Route::middleware("auth")->prefix("/admin")->group(function(){
     Route::get("/showMessage",function(){
         return view("auths.showMessage");
     })->name("showMessage");
+    Route::prefix('users')->group(function(){
+        Route::get("list",[UserController::class,"listGroupMember"])->name("admin.users");
+        Route::put("list/change_status",[UserController::class,"updateUserStatus"])->name("admin.users.user_status");
+        Route::get("create",[UserController::class,"create"])->name("admin.users.create");
+        Route::post("store",[UserController::class,"store"])->name("admin.users.store");
+        Route::get("edit/{id}",[UserController::class,"editUser"])->name("admin.users.edit");
+        Route::put("update/{id}",[UserController::class,'updateUser'])->name("admin.users.update");
+        Route::delete("delete",[UserController::class,'deleteUser'])->name("admin.users.delete");
+    });
+    Route::prefix("user_catelogue")->group(function(){
+        Route::get("list",[UserCatelogueController::class,"listGroupMember"])->name("admin.user_catelogue");
+        Route::get("create",[UserCatelogueController::class,"create"])->name("admin.user_catelogue.create");
+        Route::post("user_catelogueStore",[UserCatelogueController::class,"store"])->name("admin.user_catelogue.store");
+        Route::get("{id}/edit",[UserCatelogueController::class,"edit"])->name("admin.user_catelogue.edit");
+        Route::put("{id}/update",[UserCatelogueController::class,"UserCatelogueUpdate"])->name("admin.user_catelogue.update");
+        Route::delete("/delete",[UserCatelogueController::class,"UserCatelogueDelete"])->name("admin.user_catelogue.delete");
+    });
+    Route::prefix("post-catelogue")->group(function(){
+        Route::get("list",[PostCatelogueController::class,"index"])->name("admin.post-catelogue");
+        Route::get("create",[PostCatelogueController::class,"create"])->name("admin.post-catelogue.create");
+        Route::post("post-catelogueStore",[PostCatelogueController::class,"store"])->name("admin.post-catelogue.store");
+        Route::get("{id}/edit",[PostCatelogueController::class,"editPostCatelogue"])->name("admin.post-catelogue.edit");
+        Route::put("{id}/update",[PostCatelogueController::class,"PostCatelogueUpdate"])->name("admin.post-catelogue.update");
+        Route::delete("/delete",[PostCatelogueController::class,"PostCatelogueDelete"])->name("admin.post-catelogue.delete");
+    });
+    Route::prefix("post")->group(function(){
+        Route::get("list",[PostController::class,"index"])->name("admin.post");
+        Route::get("create",[PostController::class,"create"])->name("admin.post.create");
+        Route::post("postStore",[PostController::class,"store"])->name("admin.post.store");
+        Route::get("{id}/edit",[PostController::class,"editPost"])->name("admin.post.edit");
+        Route::put("{id}/update",[PostController::class,"update"])->name("admin.post.update");
+        Route::delete("/delete",[PostController::class,"destroy"])->name("admin.post.delete");
+    });
+    Route::prefix("product")->group(function(){
+        Route::get("list",[PostController::class,"index"])->name("admin.product");
+        Route::get("create",[PostController::class,"create"])->name("admin.product.create");
+        Route::post("postStore",[PostController::class,"store"])->name("admin.product.store");
+        Route::get("{id}/edit",[PostController::class,"editPost"])->name("admin.product.edit");
+        Route::put("{id}/update",[PostController::class,"update"])->name("admin.product.update");
+        Route::delete("/delete",[PostController::class,"destroy"])->name("admin.product.delete");
+    });
+    Route::prefix("product-catelogue")->group(function(){
+        Route::get("list",[ProductCatelogueController::class,"index"])->name("admin.product_catelogue");
+        Route::get("create",[ProductCatelogueController::class,"create"])->name("admin.product_catelogue.create");
+        Route::post("postStore",[ProductCatelogueController::class,"store"])->name("admin.product_catelogue.store");
+        Route::get("{id}/edit",[ProductCatelogueController::class,"edit"])->name("admin.product_catelogue.edit");
+        Route::put("{id}/update",[ProductCatelogueController::class,"update"])->name("admin.product_catelogue.update");
+        Route::delete("/delete",[ProductCatelogueController::class,"destroy"])->name("admin.product_catelogue.delete");
+    });
+    Route::prefix("variant-catelogue")->group(function(){
+        Route::get("list",[AttributeController::class,"index"])->name("admin.variant_catelogue");
+        Route::get("create",[AttributeController::class,"create"])->name("admin.variant_catelogue.create");
+        Route::post("postStore",[AttributeController::class,"store"])->name("admin.variant_catelogue.store");
+        Route::get("{id}/edit",[AttributeController::class,"edit"])->name("admin.variant_catelogue.edit");
+        Route::put("{id}/update",[AttributeController::class,"update"])->name("admin.variant_catelogue.update");
+        Route::delete("/delete",[AttributeController::class,"destroy"])->name("admin.variant_catelogue..delete");
+    });
+    Route::prefix("variant")->group(function(){
+        Route::get("list",[AttributeValueController::class,"index"])->name("admin.variant");
+        Route::get("create",[AttributeValueController::class,"create"])->name("admin.variant.create");
+        Route::post("postStore",[AttributeValueController::class,"store"])->name("admin.variant.store");
+        Route::get("{id}/edit",[AttributeValueController::class,"edit"])->name("admin.variant.edit");
+        Route::put("{id}/update",[AttributeValueController::class,"update"])->name("admin.variant.update");
+        Route::delete("/delete",[AttributeValueController::class,"destroy"])->name("admin.variant.delete");
+    });
 });
                     
 Route::get("/",function(){
     return redirect()->route("dashboard");
 });
+Route::get("ajax/getLocaion/index",[GetLocaitonAjax::class,"index"])->name("ajax.getLocation");
+Route::put("ajax/change_status",[ChangeStatusAjax::class,"change_status"])->name("ajax.changeStatus");
