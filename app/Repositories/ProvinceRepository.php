6e6b506ea4f8133f805255664159d61f9026f0fc
TranHoan <hoantph33008@fpt.edu.vn>
2024-10-07 23:19:48 +0700

    edit

--- app/Repositories/ProvinceRepository.php
+++ app/Repositories/ProvinceRepository.php
@@ -6,7 +6,6 @@ use App\Repositories\Interfaces\ProvinceRepositoryInterface;
 use App\Models\User;
 use App\Repositories\BaseRespository;
 use App\Models\Province;
-use Illuminate\Database\Eloquent\Builder;
 /**
  * Class UserService
  * @package App\Services
@@ -18,37 +17,6 @@ class ProvinceRepository extends BaseRespository  implements ProvinceRepositoryI
         Province $model
     ){
         $this->model = $model;
-        $query = $this->model::with("promotions")
-        ->where(function(Builder $query){
-                  if(request()->has(["keywords"])){
-                    $query->where("name","like",'%'.request()->keywords . '%');
-                  }
-        })
-        ->whereHas("promotions",function(Builder $query){
-            if(request()->has(["keywords"])){
-                $query->where("name","like",'%'.request()->keywords . '%');
-              }
-        })
-        ->paginate(15)->appends(request()->query());
-        return $query;
-      
-    }
-    public function create($data){
-        $this->model::create($data);
-      }
-      public function findId($id){
-          return $this->model::find($id);
-      }
-      public function update($data, $id)
-      {
-        $this->model::where("id",$id)->update($data);
-      }
-      public function updatestatus($status,$id){
-        $user = $this->model::find($id);
-      }
-      public function delete($id){
-        $users = $this->model::find($id);
-        $users->delete();
     }
     public function findDistrictByIdProvince($idProvince){
         if($idProvince){
@@ -60,8 +28,4 @@ class ProvinceRepository extends BaseRespository  implements ProvinceRepositoryI
         }
        
     }
-    public function all(){
-        return Province::all();
-    }
 }
-
