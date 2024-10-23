1497e36864a22a582cafbf63e1f1371dab91b5a8
Xuan Thang <thangijr@gmail.com>
2024-10-23 15:01:11 +0700

    update giao diện table product

--- resources/views/backend/products/components/fillter.blade.php
+++ resources/views/backend/products/components/fillter.blade.php
@@ -10,21 +10,23 @@
             <div class="col-md-2 p-2" style="padding-right: 2px;!important">
                 <select name="trang_thai" id="" class="form-control">
                     <option value="">Tìm kiếm theo trạng thái</option>
-                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"] == 1 ) selected @endif value="1">Hoạt động</option>
-                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"] == 0) selected @endif value="0">Không hoạt động</option>
+                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"]==1 ) selected @endif value="1">Hoạt động</option>
+                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"]==0) selected @endif value="0">Không hoạt động</option>
                 </select>
             </div>
             <div class="col-md-2 p-2">
-                <div >
+                <div>
                     <input type="datetime-local" name="ngay_dang" @if(isset($_GET["ngay_dang"])) value="{{ $_GET["ngay_dang"]}}" @endif class="form-control">
                 </div>
             </div>
-            <div class="col-md-4 d-flex align-items-center" style="padding-left: 12px;">
-                <button type="submit" class="btn btn-primary seach"> 
+            <div class="col-md-4 d-flex align-items-center" style="padding-left: 12px;display: flex; justify-content: space-between;">
+                <button type="submit" class="btn btn-outline-light seach">
                     <i class="fa-solid fa-magnifying-glass"></i> Tìm kiếm
                 </button>
-                <a class="btn btn-success ml-2" href="{{route('admin.post.create')}}">Thêm mới</a>
+
+                <a class="btn btn-success ml-2" href="{{route('admin.post.create')}}" style="font-size: 13px;"><i class="fa-solid fa-plus"></i>Thêm mới</a>
             </div>
         </form>
+
     </div>
 </div>
