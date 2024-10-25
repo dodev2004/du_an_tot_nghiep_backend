050f7e8be1a722a87e2e8d03990d1a6f606be611
Xuan Thang <thangijr@gmail.com>
2024-10-25 22:53:59 +0700

    chỉnh sửa giao diện table của danh mục product , bài viết , quản lý thành viên

--- resources/views/backend/posts/components/post/fillter.blade.php
+++ resources/views/backend/posts/components/post/fillter.blade.php
@@ -1,29 +1,29 @@
 <div class="ibox-content_top">
     <div class="form_seach" style="padding-bottom: 12px">
         <form method="GET" class="row">
-            <div class="col-md-2 p-2" style="padding-right: 2px;!important">
+            <div class="col-md-2 p-2" style="padding-right: 2px !important">
                 <input type="text" class="form-control" name="ky_tu" @if(isset($_GET["ky_tu"])) value="{{$_GET['ky_tu']}}" @endif placeholder="Tìm kiếm theo tên">
             </div>
-            <div class="col-md-2 p-2" style="padding-right: 2px;!important">
+            <div class="col-md-2 p-2" style="padding-right: 2px !important">
                 <input type="text" class="form-control" name="chuyen_muc" @if(isset($_GET["chuyen_muc"])) value="{{ $_GET["chuyen_muc"]}}" @endif placeholder="Tìm kiếm theo chuyên mục">
             </div>
-            <div class="col-md-2 p-2" style="padding-right: 2px;!important">
+            <div class="col-md-2 p-2" style="padding-right: 2px  !important">
                 <select name="trang_thai" id="" class="form-control">
                     <option value="">Tìm kiếm theo trạng thái</option>
-                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"] == 1 ) selected @endif value="1">Hoạt động</option>
-                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"] == 0) selected @endif value="0">Không hoạt động</option>
+                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"]==1 ) selected @endif value="1">Hoạt động</option>
+                    <option @if(isset($_GET["trang_thai"]) && $_GET["trang_thai"]==0) selected @endif value="0">Không hoạt động</option>
                 </select>
             </div>
             <div class="col-md-2 p-2">
-                <div >
+                <div>
                     <input type="datetime-local" name="ngay_dang" @if(isset($_GET["ngay_dang"])) value="{{ $_GET["ngay_dang"]}}" @endif class="form-control">
                 </div>
             </div>
             <div class="col-md-4 d-flex align-items-center" style="padding-left: 12px;">
-                <button type="submit" class="btn btn-primary seach"> 
+                <button type="submit" class="btn btn-primary seach">
                     <i class="fa-solid fa-magnifying-glass"></i> Tìm kiếm
                 </button>
-                <a class="btn btn-success ml-2" href="{{route('admin.post.create')}}">Thêm mới</a>
+                <a class="btn btn-success ml-2" href="{{route('admin.post.create')}}"><i class="fa fa-plus"></i>Thêm mới</a>
             </div>
         </form>
     </div>
--- resources/views/backend/posts/components/post/table.blade.php
+++ resources/views/backend/posts/components/post/table.blade.php
@@ -1,64 +1,64 @@
-<table class="table table-striped table-bordered table-hover" >
+<table class="table table-striped table-bordered table-hover">
     <thead>
-    <tr>
-        <th></th>
-       <th>Tên bài viết</th>
-        <th class="text-center">Tác giả</th>
-        <th class="text-center">Trạng thái</th>
-        <th class="text-center" style="with:auto">Danh mục</th>
-        <th class="text-center">Hành động</th>
+        <tr>
+            <th></th>
+            <th>Tên bài viết</th>
+            <th class="text-center">Tác giả</th>
+            <th class="text-center">Trạng thái</th>
+            <th class="text-center" style="width:auto">Danh mục</th>
+            <th class="text-center">Hành động</th>
 
 
-    </tr>
+        </tr>
     </thead>
     <tbody>
-    @foreach ($data as $index => $post)
+        @foreach ($data as $index => $post)
         <tr>
             <td class="text-center">{{$index+1}}</td>
             <td>
                 <p style="margin-bottom: 0;font-weight: 600;font-size: 14px;">{{$post["title"]}}</p>
-                <p style="font-size: 12px;margin-bottom: 0;" >Tin tức bóng đá liên quan đến các nhóm sản phẩm</p>
+                <p style="font-size: 12px;margin-bottom: 0;">Tin tức bóng đá liên quan đến các nhóm sản phẩm</p>
                 <p style="font-size: 10px; font-weight: bold;margin-bottom:0">Ngày đăng : {{$post["created_at"]}}</p>
                 <p style="font-size: 10px; font-weight: bold;">Ngày sửa : {{$post["created_at"]}}</p>
             </td>
             <td class="text-center">
-               <p>{{$post["author"]}}</p>
+                <p>{{$post["author"]}}</p>
             </td>
-        <td class="text-center">
-            <form name="form_status" action="">
-                @csrf
-                <input type="hidden" name="attribute" value="status">
-                <input type="hidden" name="table" value="{{$table}}">
-                <input type="checkbox"  data-id="{{$post["id"]}}" @if($post["status"] == 1) checked @endif class="js-switch js-switch_{{$post["id"]}}"  style="display: none;" data-switchery="true">
-            </form>
-        </td>
-        <td>
-            <span>
-                @if($post["catelogues"]->count()> 0)
-                @foreach($post["catelogues"] as $catelogue)
-                <span class="label label-primary">{{$catelogue->name}}</span>
-                @endforeach
-                @else
-                <span class="label label-info">Chưa có chuyên mục</span>
+            <td class="text-center">
+                <form name="form_status" action="">
+                    @csrf
+                    <input type="hidden" name="attribute" value="status">
+                    <input type="hidden" name="table" value="{{$table}}">
+                    <input type="checkbox" data-id="{{$post["id"]}}" @if($post["status"]==1) checked @endif class="js-switch js-switch_{{$post["id"]}}" style="display: none;" data-switchery="true">
+                </form>
+            </td>
+            <td>
+                <span>
+                    @if($post["catelogues"]->count()> 0)
+                    @foreach($post["catelogues"] as $catelogue)
+                    <span class="label label-primary">{{$catelogue->name}}</span>
+                    @endforeach
+                    @else
+                    <span class="label label-info">Chưa có chuyên mục</span>
 
-                @endif
-            </span>
-        </td>
-        <td style="text-align: center">
-                <div style="display: flex; justify-content: center;column-gap: 5px;" >
+                    @endif
+                </span>
+            </td>
+            <td style="text-align: center">
+                <div style="display: flex; justify-content: center;column-gap: 5px;">
 
-                    <a href="{{route('admin.post.edit',$post["id"])}}" class="btn btn-info"><i class="fa fa-pencil"></i></a>
+                    <a href="{{route('admin.post.edit',$post["id"])}}" class="btn btn-info" title="Chỉnh sửa"><i class="fa fa-pencil"></i></a>
                     <form action="" method="POST" data-url="post" class="form-delete">
                         @method("DELETE")
                         @csrf
                         <input type="hidden" value="{{$post["id"]}}" name="id">
-                                <button class="btn btn-warning center"><i class="fa fa-trash-o"></i></button>
+                        <button class="btn btn-danger center" title="Xóa"><i class="fa fa-trash-o"></i></button>
                     </form>
                 </div>
 
-        </td>
-    </tr>
-    @endforeach
+            </td>
+        </tr>
+        @endforeach
 
     </tbody>
 </table>
