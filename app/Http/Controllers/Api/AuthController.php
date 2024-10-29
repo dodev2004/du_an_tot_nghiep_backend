7fb1c4edc4b1adc147144010c4bf19f0c1af04f1
TranHoan <hoantph33008@fpt.edu.vn>
2024-10-29 11:19:07 +0700

    api dangki dangnhap

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+    
+    public function register(Request $request)
+{
+    // Xác thực các dữ liệu đầu vào
+    $validator = Validator::make($request->all(), [
+        'username' => 'required|string|max:255|unique:users',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+
+    // Kiểm tra nếu xác thực không thành công
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 422);
+    }
+
+    // Tạo người dùng mới
+    $user = User::create([
+        'username' => $request->username,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    // Tạo token cho người dùng mới đăng ký
+    $token = auth()->login($user);
+
+    // Trả về thông tin người dùng và token
+    return $this->respondWithToken($token);
+}
+
+    public function login()
+    {
+        $credentials = request(['username', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+
+        ]);
+    }
+}
