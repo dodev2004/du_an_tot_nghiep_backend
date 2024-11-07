1f85b4bc60bb90329424a10c3b2b83b173a73180
nguyenngoctruongson <nguyenngoctruongson06102003@gmail.com>
2024-11-07 22:42:28 +0700

    them thong tin phan trang

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,11 +12,10 @@ class PostController extends Controller
 {
     public function index()
     {
-        // Lấy tất cả các bài viết
-        $posts = Post::where('status',1)->paginate(2)->makeHidden(['status'])->each(function($post) {
-            $post->catelogues->makeHidden(['status']);  // Ẩn trường status trong catelogues
-        });
-        return response()->json($posts->toArray(), Response::HTTP_OK);
+        // Lấy tất cả các bài viết với phân trang
+    $posts = Post::with('catelogues')->where('status', 1)->paginate(1);
+
+return response()->json($posts, Response::HTTP_OK);
     }
 
     public function store(Request $request)
