1517f8f2c7ba4fd80a79057a36a39ee43994a891
nguyenngoctruongson <nguyenngoctruongson06102003@gmail.com>
2024-11-30 13:44:54 +0700

    them thanh vien admin thi rule_id=1

--- app/Http/Controllers/backend/UserController.php
+++ app/Http/Controllers/backend/UserController.php
@@ -62,8 +62,9 @@ class UserController extends Controller
     }
     public function store(StoreUserRequest $request)
     {
-        
+
         $data = $request->except(["re-password", "_token", "avatar", "role_id"]);
+        $data['rule_id']=1;
         if ($this->users->create($data)) {
             return response()->json(["success", "Thêm mới thành công"]);
         } else {
