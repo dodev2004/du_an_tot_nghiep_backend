6b5a57e4deca42dd1b4582f3f96010406500bf95
TranHoan <hoantph33008@fpt.edu.vn>
2024-12-03 22:57:39 +0700

    update mail

---
+++ app/Mail/CancelOrderAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CancelOrderAdmin extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    public function build()
+    {
+        return $this->subject('Đơn hàng của bạn đã được hủy')
+                    ->view('auths.email.cancel')
+                    ->with([
+                        'order' => $this->order,
+                    ]);
+    }
+}
