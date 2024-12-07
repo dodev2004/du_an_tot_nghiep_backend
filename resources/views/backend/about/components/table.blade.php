6fe2474dfb73d7d8be39dea666f9cd16bcfcdfe6
TranHoan <hoantph33008@fpt.edu.vn>
2024-12-07 21:38:41 +0700

    update

--- resources/views/backend/about/components/table.blade.php
+++ resources/views/backend/about/components/table.blade.php
@@ -14,7 +14,7 @@
                 <td>{{ $index + 1 }}</td>
                 <td>{{ $item->title }}</td>
                 <td>{{ Str::limit(strip_tags($item->content), 100) }}</td>
-                <td>{{ $item->status }}</td>
+                <td>{{ $item->status == 1 ? 'Hoạt động' : $item->status }}</td>
                 <td style="text-align: center">
                     <a href="{{ route('admin.about.edit', $item["id"]) }}" class="btn btn-info">
                         <i class="fa fa-pencil"></i>
