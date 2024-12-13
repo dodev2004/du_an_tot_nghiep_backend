fc8570b572026cbf545b5009198795e41628d979
TranHoan <hoantph33008@fpt.edu.vn>
2024-12-13 21:37:39 +0700

    huyhang

--- resources/views/auths/email/cancel.blade.php
+++ resources/views/auths/email/cancel.blade.php
@@ -7,8 +7,9 @@
     <h2>Xin chào {{ $order->customer_name }},</h2>
     @foreach ($order->orderItems as $item)
     <div class="order-item">
-        <p>Morden Home đã nhận được thông báo rằng đơn hàng <b>{{ $item->product_name }}</b> </p>
-        <p>Chúng tôi xin gửi lại số tiền của đơn hàng {{ number_format($order->final_amount, 0, ',', '.') }} VND do không hoàn thành</p>
+        <p>Morden Home đã nhận được thông báo rằng đơn hàng {{ $item->product_name }} đã giao không thành công </p>
+        <p>Lý do: {{ $mesage ?? 'Hoàn hàng' }}</p>
+        <p>Chúng tôi xin gửi lại số tiền của đơn hàng là {{ number_format($order->final_amount, 0, ',', '.') }} VND do không hoàn thành</p>
         <p>Cảm ơn bạn đã mua sắm với Morden Home!</p>
     </div>
     @endforeach
