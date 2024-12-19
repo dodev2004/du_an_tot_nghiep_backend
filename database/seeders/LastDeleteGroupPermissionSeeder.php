470eda8bf734e0517b6f8079c4ce47832f5d9c9b
nguyenngoctruongson <nguyenngoctruongson06102003@gmail.com>
2024-12-19 23:40:10 +0700

    xoa ban ghi ve nhom quen

---
+++ database/seeders/LastDeleteGroupPermissionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LastDeleteGroupPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Xóa các bản ghi trong bảng permissions có group_permission_id = 2
+        DB::table('permissions')->where('group_permission_id', 2)->delete();
+
+        // Xóa bản ghi có id = 2 trong bảng group_permission
+        DB::table('group_permission')->where('id', 2)->delete();
+
+        $this->command->info('Đã xoá các bản ghi liên quan trong bảng permissions và group_permission.');
+    }
+}
